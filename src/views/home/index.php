4b8c71c6f8a73579c737dbabbd735517263cf034
frencho <preadrianrodriguez@gmail.com>
2024-12-31 15:02:51 -0600

    [ Captures added ]

--- src/views/home/index.php
+++ src/views/home/index.php
@@ -1,6 +1,6 @@
 <?php
 require __DIR__ . '/../../sections/home/mainHero.php';
 require __DIR__ . '/../../sections/home/banner.php';
-require __DIR__ . '/../../sections/home/Steps.php';
+require __DIR__ . '/../../sections/home/steps.php';
 require __DIR__ . '/../../sections/home/lastRooms.php';
 require __DIR__ . '/../../sections/home/ratings.php';
